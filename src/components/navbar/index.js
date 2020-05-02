519a1df6b92042154bb2b77ef753060d332645d0
Fahad Israr <fahad00cms@gmail.com>
2020-05-03 03:29:28 +0530

    @Dynamic Items in Mobile Navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -4,8 +4,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+
 import Menu from '@material-ui/icons/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import './style.css'
@@ -18,7 +17,8 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import HomeIcon from '@material-ui/icons/Home';
 import LocalLibraryIcon from '@material-ui/icons/LocalLibrary';
-import MenuListComposition from './desktop-items'
+import MenuListComposition from './desktop-items';
+import TemporaryDrawer from './drawer';
 
 export default class Navbar extends React.Component{
 
@@ -46,48 +46,10 @@ export default class Navbar extends React.Component{
 
 
 
-toggleDrawer = (anchor, open) => (event) => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-      return;
-    }
-   
-    this.setState({ right:open });
-    
-  };
-
-
-
-list = (anchor) => (
-    <div
-      className={clsx(this.classes.list, {
-        [this.classes.fullList]: anchor === 'top' || anchor === 'bottom',
-      })}
-      role="presentation"
-      onClick={this.toggleDrawer(anchor, false)}
-      onKeyDown={this.toggleDrawer(anchor, false)}
-    >
-      <List>
-        
-          <ListItem button >
-            <ListItemIcon> <HomeIcon/></ListItemIcon>
-            <ListItemText primary="Home"/>
-          </ListItem>
-       
-      </List>
-      <Divider />
-      <List>
-        
-          <ListItem button >
-            <ListItemIcon><LocalLibraryIcon/></ListItemIcon>
-            <ListItemText primary="Academics" />
-          </ListItem>
-      
-      </List>
-    </div>
-  );
 
 
     render(){
+      const arr=['Institute','Academics','People','Admission','Notification','Students','Campus Life','Virtual Academy'];
         
         return(
         <div className="navbar">
@@ -95,18 +57,20 @@ list = (anchor) => (
         <div className={this.classes.root}>
         <AppBar position="static">
             <Toolbar>
-            <IconButton edge="start" className={this.classes.menuButton} color="inherit" aria-label="menu" id="mobile_navigation" onClick={this.toggleDrawer('right', true)}>
-                <MenuIcon onClick={this.toggleDrawer('right', true)}/>
-            </IconButton>
-            <Drawer anchor={'right'} open={this.state.right} onClose={this.toggleDrawer('right', false)}>
-            {this.list('right')}
-            </Drawer>
+            <div id="mobile_navbar">
+              {/*Navbar Mobile Icons Start here */}
+            
+              <TemporaryDrawer items={arr}/>
 
+              {/*Navbar Mobile Ends here */}
+            </div>
 
             <div id="desktop_menu">
-            {['Institute','Academics','People','Admission','Notification','Students','Campus Life','Virtual Academy'].map(item=>{
+              {/*Navbar @Desktop start here*/}
+            {arr.map(item=>{
             return <MenuListComposition nav_head={item}/>
             })}
+            {/*Navbar @Desktop Ends here */}
             </div>
             
             </Toolbar>
