e09e2c6706ad8b98386d193d5bc122a300ec45d0
Saratchandraogirala <saratchandraogirala19@gmail.com>
2020-06-03 04:07:41 +0530

    added footer in changed code
    added footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css'
+import { css } from '@emotion/core';
+
+/* Footer */ 
+const Footer = () => {
+
+    return(
+        <div>
+            <footer class="footer">
+                <div >
+                    <p class='credits'>Copyright © by Web Devs Club, IIITT</p>
+                </div>
+            </footer>
+        </div>
+)}
+
+export default Footer;
