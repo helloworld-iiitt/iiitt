12dd4b97de88ef24d9aac8bcdfb2a474405cd0b4
fahad-israr <fahad00cms@gmail.com>
2020-06-06 02:26:49 +0530

    Remove autofocus from nav-menu items

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Navbar from '../../components/navbar/index';
 import PaperCard from '../../components/papercard/index'
 import MainCarousel from '../../components/carousel/index';
@@ -14,7 +15,29 @@ enable_slide_control=()=>{
 document.getElementsByClassName('carousel-root')[0].style.zIndex='auto';
 }
 
-
+componentDidMount(){
+  /*  let carousel_height= document.getElementsByClassName("carousel-root")[0].clientHeight;
+    console.log(carousel_height);
+    if(document.getElementsByClassName('v_marquee')[0])document.getElementsByClassName('v_marquee')[0].style.height = `${carousel_height}px`;*/
+    
+    window.requestAnimationFrame(function(){
+    var node = ReactDOM.findDOMNode(this);
+    console.log(node);
+   
+    
+    console.log(document.defaultView.getComputedStyle(document.getElementsByClassName('carousel-root')[0], "").getPropertyValue("height"));
+    console.log(document.getElementsByClassName('carousel-slider'));
+   // document.getElementsByClassName('v_marquee')[0].style.height = document.defaultView.getComputedStyle(document.getElementsByClassName('carousel-root')[0], "").getPropertyValue("height");
+    
+        
+    });
+}
+componentWillUpdate(){
+    
+}
+componentDidUpdate(){
+   
+    }
     render(){
     
     
