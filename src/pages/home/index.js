019edcc37723225ea8445086959462ebdf4ec44a
fahad-israr <fahad00cms@gmail.com>
2020-06-06 04:25:14 +0530

    Code clean up

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -15,29 +15,7 @@ enable_slide_control=()=>{
 document.getElementsByClassName('carousel-root')[0].style.zIndex='auto';
 }
 
-componentDidMount(){
-  /*  let carousel_height= document.getElementsByClassName("carousel-root")[0].clientHeight;
-    console.log(carousel_height);
-    if(document.getElementsByClassName('v_marquee')[0])document.getElementsByClassName('v_marquee')[0].style.height = `${carousel_height}px`;*/
-    
-    window.requestAnimationFrame(function(){
-    var node = ReactDOM.findDOMNode(this);
-    console.log(node);
-   
-    
-    console.log(document.defaultView.getComputedStyle(document.getElementsByClassName('carousel-root')[0], "").getPropertyValue("height"));
-    console.log(document.getElementsByClassName('carousel-slider'));
-   // document.getElementsByClassName('v_marquee')[0].style.height = document.defaultView.getComputedStyle(document.getElementsByClassName('carousel-root')[0], "").getPropertyValue("height");
-    
-        
-    });
-}
-componentWillUpdate(){
-    
-}
-componentDidUpdate(){
-   
-    }
+
     render(){
     
     
