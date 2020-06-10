0b51925d549ef3c05d6fb331e8bc932fa303a3c7
Dinesh Vikram V <astrodini@gmail.com>
2020-06-10 17:33:07 +0530

    Update PersonCard

--- src/components/person_card/index.js
+++ src/components/person_card/index.js
@@ -1,10 +1,7 @@
 import React, { useState } from 'react';
 import { Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-
 import './styles.css'
-
-
 
 const PersonCard = (props) => {
     const [content,setContent] = useState(props.researchArea.slice(0,100));
