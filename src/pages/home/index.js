32a1e2fe751696a98865fabfc3aa453d2c4c77dc
dinskid <astrodini@gmail.com>
2020-06-15 19:00:46 +0530

    Get carousel images from json and fix unused imports

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -9,41 +9,38 @@ import Footer from '../../components/footer/index'
 import Divider from '@material-ui/core/Divider';
 import news from '../../json/news.json';
 import './style.css'
+import carouselData from '../../json/homeCarousel.json'
 export default class Home extends React.Component{
 
-enable_slide_control=()=>{
-document.getElementsByClassName('carousel-root')[0].style.zIndex='auto';
-}
-
+    enable_slide_control=()=>{
+      document.getElementsByClassName('carousel-root')[0].style.zIndex='auto';
+    }
 
     render(){
-    
-    
-
         return(
             <>
 
             <Navbar />
-            <div onMouseOver={this.enable_slide_control} className="only_on_desktop customeflex"> 
+            <div onMouseOver={this.enable_slide_control} className="only_on_desktop customeflex">
 
               <MissionVision />
-              <MainCarousel images={[{'name':'IIIT Trichy','path':'orion.jpg'},{'name':'#Ha[.sh] \'19' ,'path':'hash.jpg'},{'name':'director','path':'003.jpeg'}]}/>
-              <Marquee/> 
+              <MainCarousel images={carouselData.data}/>
+              <Marquee/>
               <br/>
               <br/>
             </div>
-          
-            <div className="only_on_desktop customeflex" id="info">          
+
+            <div className="only_on_desktop customeflex" id="info">
               <PaperCard title="News" items={news.data}/>
               <PaperCard title="Events" items={news.data}/>
               <PaperCard title="Notice" items={news.data}/>
             </div>
             <div onMouseOver={this.enable_slide_control} className="only_on_mobile customeflex">
-                <MainCarousel images={[{'name':'IIIT Trichy','path':'orion.jpg'},{'name':'#Ha[.sh] \'19' ,'path':'hash.jpg'},{'name':'director','path':'003.jpeg'}]}/>
+                <MainCarousel images={carouselData.data}/>
                 <MissionVision />
                 <Marquee/>
-               
-                <div className="only_on_mobile customeflex" id="info_mob">   
+
+                <div className="only_on_mobile customeflex" id="info_mob">
                 <PaperCard title="News" items={news.data}/>
                 <PaperCard title="Notice" items={news.data}/>
                 <PaperCard title="Events" items={news.data}/>
