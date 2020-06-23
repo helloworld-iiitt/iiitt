622e7a5ccca47e335c666d44e2298250e8f2e52c
Dinesh Vikram V <astrodini@gmail.com>
2020-06-23 23:26:26 +0530

    Fix AdmissionContact and Add padding to Admission Pages

--- src/pages/admissionforms/index.js
+++ src/pages/admissionforms/index.js
@@ -28,33 +28,37 @@ export default function AdmissionForms() {
   const classes = createStyles()
   const reportingDetails = 'ReportingDetails_IIITT_2019-20.pdf'
   const admissionCancellation = 'CancellationForm.docx'
-	return (
-		<>
+  return (
+    <>
       <Navbar />
-      <Grid container>
-        <Grid item xs={false} sm={1} />
-        <Grid item xs={12} sm={10} >
-          <Typography variant="h2" component="h2" gutterBottom className={classes.themeText}>
-            <Box component="span" fontWeight={380}>
-              Admission Forms
-            </Box>
-          </Typography>
-
-          <ul className="doclist">           
-            <li>
-              <a href={require(`../../docs/${reportingDetails}`)} download={`${reportingDetails}`} className={classes.link}>
-              {reportingDetails}
-              </a>
-            </li>
-            <li>
-              <a href={require(`../../docs/${admissionCancellation}`)} download={`${admissionCancellation}`} className={classes.link}>
-                {admissionCancellation}
-             </a>
-            </li>
-          </ul>
-        </Grid>
+      <Grid container className={classes.container}>
+	<Grid item xs={false} sm={1} />
+	<Grid item xs={12} sm={10} >
+	  <Typography variant="h2" component="h2" gutterBottom className={classes.themeText}>
+	    <Box component="span" fontWeight={380}>
+	      Admission Forms
+	    </Box>
+	  </Typography>
+	  <section>
+	    <Typography variant="h5" className={classes.themeText}>
+	      Reporting Details
+	    </Typography>
+	    <a href={require(`../../docs/${reportingDetails}`)} download={`${reportingDetails}`} className={classes.link}>
+	      {reportingDetails}
+	    </a>
+	  </section>
+	  <br />
+	  <section>
+	    <Typography variant="h5" className={classes.themeText}>
+	      Reporting Details
+	    </Typography>
+	    <a href={require(`../../docs/${admissionCancellation}`)} download={`${admissionCancellation}`} className={classes.link}>
+	      {admissionCancellation}
+	    </a>
+	  </section>
+	</Grid>
       </Grid>
       <Footer />
     </>
-    )
+  )
 }
