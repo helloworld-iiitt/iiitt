69b43bdc4b167b9248aa69a368ead0520ac04cf2
Dinesh Vikram V <astrodini@gmail.com>
2020-07-16 02:00:38 +0530

    Fix overflow of text

--- src/components/person_card/index.js
+++ src/components/person_card/index.js
@@ -6,7 +6,7 @@ import "./styles.css";
 const useStyle = makeStyles({
   show: {
     cursor: "pointer",
-    fontSize: "0.8em",
+    fontSize: "0.6em",
     background: "#3f51b5",
     padding: "0.2rem",
     borderRadius: "5px",
@@ -18,91 +18,94 @@ const useStyle = makeStyles({
     color: "#424242",
     fontSize: "1em",
   },
+  researchArea: {
+    fontSize: "0.85em"
+  }
 });
 
 const PersonCard = (props) => {
+  const sliceLimit = 80;
   const classes = useStyle();
-  const [content, setContent] = useState(props.researchArea.slice(0, 100));
+  const [content, setContent] = useState(props.researchArea.slice(0, sliceLimit));
 
   const showLess = (e) => {
-    setContent(props.researchArea.slice(0, 100));
+    setContent(props.researchArea.slice(0, sliceLimit));
   };
 
   const showMore = (e) => {
     setContent(props.researchArea);
   };
 
-  console.log(`../../images/people/${props.src_type}/${props.src}`)
   return (
     <>
       <div className={`container`}>
-        <div className="pic">
-          <img
-            src={require(`../../images/people/${props.src_type}/${props.src}`)}
-            alt={props.name}
-            className="image"
-          />
-        </div>
-        <div className="content">
-          <Typography variant="h5">{props.name}</Typography>
-          <Typography variant="h6" className={classes.designation} gutterBottom>
-            {props.designation}
-          </Typography>
-          {props.researchArea && (
-            <>
-              <Typography variant="body2" gutterBottom>
-                {content}
-                {props.researchArea.length !== content.length &&
-                  props.researchArea.length > 100 &&
-                  "..."}
-              </Typography>
-              {props.researchArea.length !== content.length && (
-                <Typography
-                  onClick={showMore}
-                  variant="span"
-                  className={classes.show}
-                >
-                  Show More
-                </Typography>
-              )}
-              {props.researchArea.length === content.length &&
-                props.researchArea.length > 100 && (
-                  <Typography
-                    onClick={showLess}
-                    variant="span"
-                    className={classes.show}
-                  >
-                    Show Less
-                  </Typography>
-                )}
-            </>
-          )}
-          {/* <Typography variant="body2" gutterBottom>
-                        {content}{(props.researchArea.length !== content.length && props.researchArea.length>100) && '...'}
-                    </Typography>
-                    {
-                    (props.researchArea.length !== content.length) &&
-                    <Typography onClick={showMore} variant="span" className={classes.show}>
-                        Show More
-                    </Typography>
-                    }
-                    {
-                    (props.researchArea.length === content.length && props.researchArea.length>100) &&
-                    <Typography onClick={showLess} variant="span" className={classes.show}>
-                        Show Less
-                    </Typography>
-                    } */}
-          <br />
-          <Typography variant="body2">
-            <a href={`mailto:${props.emailID}`} className="email">
-              <span aria-label="mailing" role="img">
-                &#128231;
-              </span>
-              &nbsp;
-              {props.emailID}
-            </a>
-          </Typography>
-        </div>
+	<div className="pic">
+	  <img
+	    src={require(`../../images/people/${props.src_type}/${props.src}`)}
+	    alt={props.name}
+	    className="image"
+	  />
+	</div>
+	<div className="content">
+	  <Typography variant="h5">{props.name}</Typography>
+	  <Typography variant="h6" className={classes.designation} gutterBottom>
+	    {props.designation}
+	  </Typography>
+	  {props.researchArea && (
+	    <>
+	      <Typography variant="body2" gutterBottom className={classes.researchArea}>
+		{content}
+		{props.researchArea.length !== content.length &&
+		    props.researchArea.length > sliceLimit &&
+		    "..."}
+	      </Typography>
+	      {props.researchArea.length !== content.length && (
+		<Typography
+		  onClick={showMore}
+		  variant="span"
+		  className={classes.show}
+		>
+		  Show More
+		</Typography>
+	      )}
+	      {props.researchArea.length === content.length &&
+		  props.researchArea.length > sliceLimit && (
+		    <Typography
+		      onClick={showLess}
+		      variant="span"
+		      className={classes.show}
+		    >
+		      Show Less
+		    </Typography>
+		  )}
+	    </>
+	  )}
+	  {/* <Typography variant="body2" gutterBottom>
+			{content}{(props.researchArea.length !== content.length && props.researchArea.length>sliceLimit) && '...'}
+		    </Typography>
+		    {
+		    (props.researchArea.length !== content.length) &&
+		    <Typography onClick={showMore} variant="span" className={classes.show}>
+			Show More
+		    </Typography>
+		    }
+		    {
+		    (props.researchArea.length === content.length && props.researchArea.length>sliceLimit) &&
+		    <Typography onClick={showLess} variant="span" className={classes.show}>
+			Show Less
+		    </Typography>
+		    } */}
+	  <br />
+	  <Typography variant="body2">
+	    <a href={`mailto:${props.emailID}`} className="email">
+	      <span aria-label="mailing" role="img">
+		&#128231;
+	      </span>
+	      &nbsp;
+	      {props.emailID}
+	    </a>
+	  </Typography>
+	</div>
       </div>
     </>
   );
