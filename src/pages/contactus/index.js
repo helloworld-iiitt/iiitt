bc02d7d1f2b79af9c7259c363ef4c64ef0aae0bf
Dinesh Vikram V <astrodini@gmail.com>
2020-08-23 13:07:00 +0530

    fix map embedding in contact us

--- src/pages/contactus/index.js
+++ src/pages/contactus/index.js
@@ -88,7 +88,7 @@ export default function ContactUs() {
 	    </Typography>
 	    {
 	      <div className={classes.map}>
-		<Iframe src="http://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9221.063529846686!2d78.65288163433289!3d10.788158321446737!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3baa5ff42ca07773%3A0x8142614ddb45a74c!2sOxford%20Engineering%20College!5e0!3m2!1sen!2sin!4v1597141616856!5m2!1sen!2sin" width="600" height="450" frameBorder="0" style="border:0;" allowFullScreen="" aria-hidden="false" tabIndex="0"></Iframe>
+		<Iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9221.063529846686!2d78.65288163433289!3d10.788158321446737!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3baa5ff42ca07773%3A0x8142614ddb45a74c!2sOxford%20Engineering%20College!5e0!3m2!1sen!2sin!4v1597141616856!5m2!1sen!2sin" width="600" height="450" frameBorder="0" style="border:0;" allowFullScreen="" aria-hidden="false" tabIndex="0"></Iframe>
 	      </div>
 	    }
 	  </section>
