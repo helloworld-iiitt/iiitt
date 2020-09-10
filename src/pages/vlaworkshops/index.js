b463f79aabe61eac3c1c26ba179c8781f68824b1
Nitish Kumar <nitishkumargenius@gmail.com>
2020-09-10 19:17:28 +0530

    Applied indentation for vlasummary, vlavideos, vlaworkshoproposal, vlaworkshoproposalitem, vlaworkshops page

--- src/pages/vlaworkshops/index.js
+++ src/pages/vlaworkshops/index.js
@@ -1,10 +1,24 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from '../../components/navbar/index'
-import Footer from '../../components/footer/index'
-import { Link } from 'react-router-dom'
-import { TableContainer, Table, TableBody, TableHead, TableRow, TableCell, Card, Typography, Grid, Box, CardMedia, CardContent, Paper } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
-import './styles.css'
+import React, { useEffect, useState } from "react";
+import Navbar from "../../components/navbar/index";
+import Footer from "../../components/footer/index";
+import { Link } from "react-router-dom";
+import {
+  TableContainer,
+  Table,
+  TableBody,
+  TableHead,
+  TableRow,
+  TableCell,
+  Card,
+  Typography,
+  Grid,
+  Box,
+  CardMedia,
+  CardContent,
+  Paper,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import "./styles.css";
 
 const createStyles = makeStyles({
   themeText: {
@@ -16,28 +30,28 @@ const createStyles = makeStyles({
     paddingTop: "1rem",
     fontWeight: "500",
     width: "auto",
-    '&:hover': {
+    "&:hover": {
       textDecoration: "underline",
-      color: "blueviolet"
-    }
+      color: "blueviolet",
+    },
   },
   tableRow: {
-    '&:nth-of-type(even)': {
-      backgroundColor: "rgba(0,0,0,0.10)"
-    }
+    "&:nth-of-type(even)": {
+      backgroundColor: "rgba(0,0,0,0.10)",
+    },
   },
   tableHead: {
     fontWeight: "900",
     backgroundColor: "#3f51b5",
-    color: "white"
+    color: "white",
   },
   tableCell: {
-    fontSize: "1.2rem"
+    fontSize: "1.2rem",
   },
   table: {
-    marginBottom: "1.5rem"
+    marginBottom: "1.5rem",
   },
-})
+});
 
 export default function VlaWorkshops() {
   useEffect(() => {
@@ -50,61 +64,88 @@ export default function VlaWorkshops() {
   }, []);
   const [workshops, setWorkshops] = useState(undefined);
   useEffect(() => {
-    import('../../json/vlaworkshops.json')
-      .then((data)=> setWorkshops(data.data))
-  }, [])
+    import("../../json/vlaworkshops.json").then((data) =>
+      setWorkshops(data.data)
+    );
+  }, []);
 
-  const classes = createStyles()
-  var ctr = 0
+  const classes = createStyles();
+  var ctr = 0;
   return (
     <div className="page-container">
-      <Navbar src="vla_navbar.json" homeRoute="/vla"/>
+      <Navbar src="vla_navbar.json" homeRoute="/vla" />
       <Grid container className={classes.container}>
         <Grid item xs={false} sm={1} />
-        <Grid item xs={12} sm={10} >
-          <Typography variant="h2" component="h2" gutterBottom className={classes.themeText}>
+        <Grid item xs={12} sm={10}>
+          <Typography
+            variant="h2"
+            component="h2"
+            gutterBottom
+            className={classes.themeText}
+          >
             <Box component="span" fontWeight={380}>
               Workshops Planned and Completed
             </Box>
           </Typography>
-          {
-            workshops&&
-            <TableContainer component={Paper} className={classes.table} gutterBottom>
+          {workshops && (
+            <TableContainer
+              component={Paper}
+              className={classes.table}
+              gutterBottom
+            >
               <Table>
                 <TableHead>
                   <TableRow>
-                    <TableCell className={`${classes.tableHead} ${classes.tableCell}`}>
+                    <TableCell
+                      className={`${classes.tableHead} ${classes.tableCell}`}
+                    >
                       S. No.
                     </TableCell>
-                    <TableCell className={`${classes.tableHead} ${classes.tableCell}`}>
+                    <TableCell
+                      className={`${classes.tableHead} ${classes.tableCell}`}
+                    >
                       Name of the Programme
                     </TableCell>
-                    <TableCell className={`${classes.tableHead} ${classes.tableCell}`}>
+                    <TableCell
+                      className={`${classes.tableHead} ${classes.tableCell}`}
+                    >
                       Co-ordinator(s) Name
                     </TableCell>
-                    <TableCell className={`${classes.tableHead} ${classes.tableCell}`}>
+                    <TableCell
+                      className={`${classes.tableHead} ${classes.tableCell}`}
+                    >
                       Start Date
                     </TableCell>
-                    <TableCell className={`${classes.tableHead} ${classes.tableCell}`}>
+                    <TableCell
+                      className={`${classes.tableHead} ${classes.tableCell}`}
+                    >
                       End Date
                     </TableCell>
-                    <TableCell className={`${classes.tableHead} ${classes.tableCell}`}>
+                    <TableCell
+                      className={`${classes.tableHead} ${classes.tableCell}`}
+                    >
                       No. Of Participants
                     </TableCell>
-                    <TableCell className={`${classes.tableHead} ${classes.tableCell}`}>
+                    <TableCell
+                      className={`${classes.tableHead} ${classes.tableCell}`}
+                    >
                       Videos
                     </TableCell>
-                    <TableCell className={`${classes.tableHead} ${classes.tableCell}`}>
+                    <TableCell
+                      className={`${classes.tableHead} ${classes.tableCell}`}
+                    >
                       Brochure
                     </TableCell>
-                    <TableCell className={`${classes.tableHead} ${classes.tableCell}`}>
+                    <TableCell
+                      className={`${classes.tableHead} ${classes.tableCell}`}
+                    >
                       Summary & Feedback
                     </TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {workshops.map(workshop => {
-                    ctr++
+                  {workshops.map((workshop) => {
+                    ctr++;
                     return (
                       <TableRow className={classes.tableRow}>
                         <TableCell className={classes.tableCell}>
@@ -123,47 +164,59 @@ export default function VlaWorkshops() {
                           {workshop.endDate}
                         </TableCell>
                         <TableCell className={classes.tableCell}>
-                          {
-                            workshop.participantsLink.trim() &&
-                            <Link to={{ pathname: workshop.participantsLink.trim() }} className={classes.link}>
+                          {workshop.participantsLink.trim() && (
+                            <Link
+                              to={{
+                                pathname: workshop.participantsLink.trim(),
+                              }}
+                              className={classes.link}
+                            >
                               {workshop.participants}
                             </Link>
-                          }
+                          )}
                         </TableCell>
                         <TableCell className={classes.tableCell}>
-                          {
-                            workshop.videos.trim() &&
-                            <Link to={{ pathname: workshop.videos }} className={classes.link}>
+                          {workshop.videos.trim() && (
+                            <Link
+                              to={{ pathname: workshop.videos }}
+                              className={classes.link}
+                            >
                               Videos
                             </Link>
-                          }
+                          )}
                         </TableCell>
                         <TableCell className={classes.tableCell}>
-                          {
-                            workshop.brochure.trim() && <a href={`../../docs/vla/${workshop.brochure}`} className={classes.link} download={workshop.brochure}>
+                          {workshop.brochure.trim() && (
+                            <a
+                              href={`../../docs/vla/${workshop.brochure}`}
+                              className={classes.link}
+                              download={workshop.brochure}
+                            >
                               Brochure
                             </a>
-                          }
+                          )}
                         </TableCell>
                         <TableCell className={classes.tableCell}>
-                          {
-                            workshop.summary.trim() &&
-                            <Link to={{ pathname: workshop.summary }} className={classes.link}>
+                          {workshop.summary.trim() && (
+                            <Link
+                              to={{ pathname: workshop.summary }}
+                              className={classes.link}
+                            >
                               Summary & Feedback
                             </Link>
-                          }
+                          )}
                         </TableCell>
                       </TableRow>
-                    )
+                    );
                   })}
                 </TableBody>
               </Table>
             </TableContainer>
-          }
+          )}
         </Grid>
         <Grid item xs={false} sm={1} />
       </Grid>
       <Footer />
     </div>
-  )
+  );
 }
