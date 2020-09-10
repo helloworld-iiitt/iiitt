50d180f8069b37373652262863ece74fce2404b5
Nitish Kumar <nitishkumargenius@gmail.com>
2020-09-10 18:30:57 +0530

    Applied indentation for department, director, faculty, faqs, fc, festivals page

--- src/pages/departments/index.js
+++ src/pages/departments/index.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from '../../components/navbar/index'
-import Footer from '../../components/footer/index'
-import { Typography, Grid, Box } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
+import React, { useEffect, useState } from "react";
+import Navbar from "../../components/navbar/index";
+import Footer from "../../components/footer/index";
+import { Typography, Grid, Box } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 const createStyles = makeStyles({
   container: {
-    padding: "1rem 1rem"
+    padding: "1rem 1rem",
   },
   themeText: {
     color: "#3f51b5",
@@ -15,9 +15,9 @@ const createStyles = makeStyles({
     padding: "1rem 0",
   },
   dept_desc: {
-    paddingLeft: "0.5rem"
-  }
-})
+    paddingLeft: "0.5rem",
+  },
+});
 
 export default function Departments() {
   useEffect(() => {
@@ -30,47 +30,60 @@ export default function Departments() {
     };
   }, []);
 
-  const [depts, setDepts] = useState(undefined)
+  const [depts, setDepts] = useState(undefined);
 
   useEffect(() => {
-    import('../../json/departments.json')
-      .then(d => {
-	setDepts(d.data)
-      })
-  }, [])
+    import("../../json/departments.json").then((d) => {
+      setDepts(d.data);
+    });
+  }, []);
 
-  const classes = createStyles()
+  const classes = createStyles();
 
   return (
     <div className="page-container">
       <Navbar />
       <Grid container className={classes.container}>
-	<Grid item xs={false} sm={1} />
-	<Grid item xs={12} sm={10} >
-	  <Typography variant="h2" component="h2" gutterBottom className={{...classes.themeText}} className={classes.themeText}>
-	    <Box component="span" fontWeight={380}>
-	      Departments
-	    </Box>
-	  </Typography>
-	  {
-	    depts&&depts.map(dept => {
-	      return (
-		<section className={classes.sectionPadding}>
-		  <Typography variant="h5" className={classes.themeText} gutterBottom>
-		    <Box component="span" fontWeight="fontWeightBold">
-		      {dept.name}
-		    </Box>
-		  </Typography>
-		  <Box component="span" fontSize="1.2em" className={classes.dept_desc} gutterBottom>
-		    {dept.description}
-		  </Box>
-		</section>
-	      )
-	    })
-	  }
-	</Grid>
+        <Grid item xs={false} sm={1} />
+        <Grid item xs={12} sm={10}>
+          <Typography
+            variant="h2"
+            component="h2"
+            gutterBottom
+            className={{ ...classes.themeText }}
+            className={classes.themeText}
+          >
+            <Box component="span" fontWeight={380}>
+              Departments
+            </Box>
+          </Typography>
+          {depts &&
+            depts.map((dept) => {
+              return (
+                <section className={classes.sectionPadding}>
+                  <Typography
+                    variant="h5"
+                    className={classes.themeText}
+                    gutterBottom
+                  >
+                    <Box component="span" fontWeight="fontWeightBold">
+                      {dept.name}
+                    </Box>
+                  </Typography>
+                  <Box
+                    component="span"
+                    fontSize="1.2em"
+                    className={classes.dept_desc}
+                    gutterBottom
+                  >
+                    {dept.description}
+                  </Box>
+                </section>
+              );
+            })}
+        </Grid>
       </Grid>
       <Footer />
     </div>
-  )
+  );
 }
--- src/pages/faculty/index.js
+++ src/pages/faculty/index.js
@@ -34,23 +34,23 @@ export default function Faculty(props) {
     var li = [];
     li.push(
       fac_data[`${grp}`].map((x) => {
-	const { name, emailID, src, designation, researchArea, id } = x;
-	const { dept, deptID } = id;
-	//console.log("Dept", dept, "key", deptID);
-	return (
-	  <Grid item xs={12} md={6} lg={4}>
-	    <PersonCard
-	      name={name}
-	      emailID={emailID}
-	      src={src}
-	      src_type='faculty'
-	      designation={designation}
-		  researchArea={researchArea}
-		  dept={dept}
-		  deptID={deptID}
-	    />
-	  </Grid>
-	);
+        const { name, emailID, src, designation, researchArea, id } = x;
+        const { dept, deptID } = id;
+        //console.log("Dept", dept, "key", deptID);
+        return (
+          <Grid item xs={12} md={6} lg={4}>
+            <PersonCard
+              name={name}
+              emailID={emailID}
+              src={src}
+              src_type="faculty"
+              designation={designation}
+              researchArea={researchArea}
+              dept={dept}
+              deptID={deptID}
+            />
+          </Grid>
+        );
       })
     );
     branches.push(li);
@@ -60,28 +60,28 @@ export default function Faculty(props) {
     <div className="page-container">
       <Navbar />
       <Grid container>
-	<Grid item xs={false} sm={1} />
-	<Grid container item xs={12} sm={10}>
-	  {branches.map((x, ind) => {
-	    return (
-	      <>
-		<Grid item sm={12}>
-		  <Typography
-		    variant="h3"
-		    component="h3"
-		    className={classes.grp}
-		  >
-		    {grps[ind]}
-		  </Typography>
-		</Grid>
-		<Grid container item spacing={1} style={{ margin: "0.5rem" }}>
-		  {x}
-		</Grid>
-	      </>
-	    );
-	  })}
-	</Grid>
-	<Grid item xs={false} sm={1} />
+        <Grid item xs={false} sm={1} />
+        <Grid container item xs={12} sm={10}>
+          {branches.map((x, ind) => {
+            return (
+              <>
+                <Grid item sm={12}>
+                  <Typography
+                    variant="h3"
+                    component="h3"
+                    className={classes.grp}
+                  >
+                    {grps[ind]}
+                  </Typography>
+                </Grid>
+                <Grid container item spacing={1} style={{ margin: "0.5rem" }}>
+                  {x}
+                </Grid>
+              </>
+            );
+          })}
+        </Grid>
+        <Grid item xs={false} sm={1} />
       </Grid>
       <Footer />
     </div>
