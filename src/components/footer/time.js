ae02f932334674ad93776ed4a298c8f13af9e987
mayankSonkar2@14 <55187056+Mayank1-cpu@users.noreply.github.com>
2020-10-08 13:37:40 +0530

    changes time proportion 12 hr to 24 hr

--- src/components/footer/time.js
+++ src/components/footer/time.js
@@ -41,23 +41,8 @@ const Time = () => {
     const getHour2 = getHour1 % 12;
 
     setgetday(getday1);
-<<<<<<< HEAD
     setgetHour(getHour1 < 10 ? `0${getHour1}` : getHour1);
     setgetminutes(getminutes1 < 10 ? `0${getminutes1}` : getminutes1);
-=======
-
-    setgetHour(getHour2 < 10 ? `0${getHour2}` : getHour2);
-
-    if (getHour1 / 12 <= 1) {
-      setgetminutes(
-        getminutes1 > 10 ? `${getminutes1}am` : `0${getminutes1}am`
-      );
-    } else {
-      setgetminutes(
-        getminutes1 > 10 ? `${getminutes1}pm` : `0${getminutes1}pm`
-      );
-    }
->>>>>>> 5db48f3ca4448ba745177027b6f8ed2eabd99ff5
     setgetDate(getDate1);
     setgetMonth(getMonth1);
     setgetFullYear(getFullYear1);
