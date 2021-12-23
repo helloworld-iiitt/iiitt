75273f4693f291d76a1579ff3de9b40327ee2122
mayankSonkar2@14 <55187056+Mayank1-cpu@users.noreply.github.com>
2021-12-23 10:59:15 +0530

    add web dev club page

---
+++ src/pages/webDevClub/components/Alumini/AluminiData.js
@@ -0,0 +1,13 @@
+export const AluminiData = [
+    {   
+        key:"1",
+        name:"Fahad Israr",
+        image:require("../../media/Fahad_israr.jfif"),
+        about:"I <3 Open Sorce.",
+        skills:"Java, QUARKUS, JBANG, PICOCLI, REACT-JS, NODE-JS, MYSQL, HTML, CSS, DRUPAL 8",
+        achievements:"GOOGLE SUMMER OF CODE with Redhat(jBoss), IIT BOMBAY (FOSSEE FELLOWSHIP ’19), LENSHOOD (Software Development Intern), IOTREK (Software Development Intern)",
+        website:"https://fahad-israr.github.io/profile/#aboutMe",
+        github:"https://github.com/fahad-israr",
+        linkdin:"https://www.linkedin.com/in/fahad00cms/",
+    }
+]
