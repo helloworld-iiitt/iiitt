5671c5c159be3e3b3de5ece7878bf6b38f2c0836
sarthak503 <sarthak.kumar503@gmail.com>
2022-01-14 20:34:11 +0530

    removed course reg file

--- src/pages/webDevClub/components/ClubMembers/PersonData.js
+++ src/pages/webDevClub/components/ClubMembers/PersonData.js
@@ -1,26 +1,52 @@
 export const MembersData = [
-    {  image: require("../../media/Dinesh_vikram.svg"),
-       name: "Dinesh Vikram V",
-       role: "Fourth Year",
-       about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br/>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi",
-       email: "#",
-       linkdin: "https://www.linkedin.com/in/dinesh-vikram-venkatesh-8b1a2516b/",
-       github: "https://github.com/dinskid"
-    },
-     {  image: require("../../media/nitish_kumar.jpeg"),
-       name: "Nitish Kumar",
-       role: "Fourth Year",
-       about: "A problem-solver who started the journey long back. After gathering some experiences, I started exploring web development, where I learned enormous things, It’s been a great journey for me and, I’m still learning.",
-       email: "#",
-       linkdin: "https://www.linkedin.com/in/thisisnitish/",
-       github: "https://github.com/thisisnitish"
-    },
-    {  image: require("../../media/Mayank_sonkar.jpeg"),
+  {
+    image: require("../../media/Dinesh_vikram.svg"),
+    name: "Dinesh Vikram V",
+    role: "Fourth Year",
+    about:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br/>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi",
+    email: "#",
+    linkdin: "https://www.linkedin.com/in/dinesh-vikram-venkatesh-8b1a2516b/",
+    github: "https://github.com/dinskid",
+  },
+  {
+    image: require("../../media/nitish_kumar.jpeg"),
+    name: "Nitish Kumar",
+    role: "Fourth Year",
+    about:
+      "A problem-solver who started the journey long back. After gathering some experiences, I started exploring web development, where I learned enormous things, It’s been a great journey for me and, I’m still learning.",
+    email: "#",
+    linkdin: "https://www.linkedin.com/in/thisisnitish/",
+    github: "https://github.com/thisisnitish",
+  },
+  {
+    image: require("../../media/Mayank_sonkar.jpeg"),
     name: "Mayank Sonkar",
     role: "Third Year",
-    about: "I started coding to fossick distinct fields and opportunities but as I went intensively, I fell in love with the process. I enhanced my wealth of experience designing and developing a web application.",
+    about:
+      "I started coding to fossick distinct fields and opportunities but as I went intensively, I fell in love with the process. I enhanced my wealth of experience designing and developing a web application.",
     email: "mailto:mkmayankkumar2014@gmail.com",
     linkdin: "https://www.linkedin.com/in/mayank-sonkar-17a8401b6/ ",
-    github: "https://github.com/mayank2021"
-   }
-]
+    github: "https://github.com/mayank2021",
+  },
+  {
+    image: require("../../media/Sarthak.jpeg"),
+    name: "Sarthak Kumar",
+    role: "Second Year",
+    about:
+      "I began my coding journey in my school days and with time I learnt a lot in the field of Tech. I really want to work in this industry and contribute for its advancement",
+    email: "mailto:sarthak.kmr503@gmail.com",
+    linkdin: "https://www.linkedin.com/in/sarthak-kumar503/",
+    github: "https://github.com/sarthak503",
+  },
+  {
+    image: require("../../media/jagmohan.jpeg"),
+    name: "Jagmohan Dixit",
+    role: "Second Year",
+    about:
+      "This is Jagmohan Dixit from ECE Department. I am passionate about coding and development and always curious to explore new technologies and a good learner.",
+    email: "mailto:jagmohandixir686@gmail.com",
+    linkdin: "https://www.linkedin.com/in/jagmohandixit/",
+    github: "https://github.com/Jagmohan-Dixit",
+  },
+];
