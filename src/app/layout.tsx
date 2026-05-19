234d42b2ab9982c22f418b61e32f523b94fa4b4c
avinash-pnvss <pnvss.avinash@gmail.com>
2026-05-19 20:26:36 +0530

    library page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,9 @@
 import { ReactNode } from "react";
 import Header from "../components/header/Header";
 import Navbar from "../components/navbar/navbar";
-import Footer from "../components/footer/Footer"
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-
+import Footer from "../components/footer/Footer";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 interface LayoutProps {
   children: ReactNode;
@@ -13,13 +12,19 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <html>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        />
+      </head>
       <body>
-      <Header />
-      <div className="page-container">
-      <Navbar/>
-      <main>{children}</main>
-      <Footer />
-      </div>
+        <Header />
+        <div className="page-container">
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
