2aec8fa3b0af6c6f9bca3808172ebc539855c890
jkokila2025 <kokilaj@iiitt.ac.in>
2026-06-15 17:13:38 +0530

    Update page.tsx
    updated_2026

--- src/app/admission_pg/page.tsx
+++ src/app/admission_pg/page.tsx
@@ -95,21 +95,20 @@ const Admission_pg: React.FC = () => {
                         <Box key={program.id} >
                             <h3><strong>{program.title}</strong></h3>
                             <hr></hr>
-                            {program.items.map((item: any, index: number) => (
-                                <p key={index}>
-                                     {item.url ? (
-                                                <a
-                                                 target="_blank"
-                                                 rel="noopener noreferrer"
-                                                 href={ validURL(item.url)? item.url : `${nextConfig?.env?.DOCUMENT}/${item.url}`}
-                                                className={styles.link}
-                                                 > {item.label}
-                                                </a>
-                                                ) : (
-                                             <span>{item.label}</span>
-                                             )}
-                                   </p>
-                                ))}
+                           {program.items.map((item: any, index: number) => (
+                          <p key={index}>
+                              <a
+                               target={item.url ? "_blank" : "_self"}
+                               rel="noopener noreferrer"
+                               href={item.url ? (validURL(item.url) ? item.url: `${nextConfig?.env?.DOCUMENT}/${item.url}`): "#"
+                         }
+                          onClick={!item.url ? (e) => e.preventDefault() : undefined}
+                         className={styles.link}
+                          >
+                          {item.label}
+                         </a>
+                         </p>
+                         ))}
 
                             <br />
                         </Box>
