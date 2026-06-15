38fafeec44ce0e6e6af1b8f1ac097238fb088308
jkokila2025 <kokilaj@iiitt.ac.in>
2026-06-15 16:32:28 +0530

    Update page.tsx
    upadated_to check

--- src/app/admission_pg/page.tsx
+++ src/app/admission_pg/page.tsx
@@ -92,22 +92,26 @@ const Admission_pg: React.FC = () => {
                     </Typography>
 
 
-                    {admissionData.programdetails.map((program: any) => (
-                        <Box key={program.id} >
-                            <h3><strong>{program.title}</strong></h3>
-                            <hr></hr>
-                            {program.items.map((item: any, index: number) => (
-                                <p key={index}>
-                                    <a
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        href={validURL(item.url) ? item.url : `${nextConfig?.env?.DOCUMENT}/${item.url}`}
-                                        className={styles.link}
-                                    >
-                                        {item.label}
-                                    </a>
-                                </p>
-                            ))}
+                   {program.items.map((item: any, index: number) => (
+    <p key={index}>
+        {item.url ? (
+            <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={
+                    validURL(item.url)
+                        ? item.url
+                        : `${nextConfig?.env?.DOCUMENT}/${item.url}`
+                }
+                className={styles.link}
+            >
+                {item.label}
+            </a>
+        ) : (
+            <span>{item.label}</span>
+        )}
+    </p>
+))}
                             <br />
                         </Box>
                     ))}
